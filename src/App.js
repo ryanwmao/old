06b988f334d18f8e51638efb7d2edf4324f362c8
rwm275 <rwm275@cornell.edu>
2022-02-03 18:38:23 -0500

    tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import Website from './components/Website';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="p-6 items-center justify-center">
+      <Website />
     </div>
   );
 }
