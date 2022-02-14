678243803168859ed4902d931e4b01031f8765f7
rwm275 <rwm275@cornell.edu>
2022-02-14 16:43:23 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Website from './components/Website';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
-
   return (
     <div className="App" class="p-6 items-center justify-center">
-      <Website />
+      <Website></Website>
     </div>
   );
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "../App.css";
+import Heart from "react-heart";
+
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+
+export default class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      level: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Heart">
+        <Heart
+          isActive={this.state.active}
+          onClick={() =>
+            this.setState({
+              level: this.state.level + Math.random() * 5,
+              active: this.state.level > 50,
+            })
+          }
+        />
+        <div className="heart2" isActive={this.state.active}>
+          {this.state.active && (
+            <span className="font-link">
+              {" "}
+              ~~ Ahjuma, Happy Valentine's Day! much love ~~{" "}
+            </span>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
